f3e7edae2879eba44a3a18682f0a18649fb46c89
Nimish Santosh <nimishsantosh107@icloud.com>
2019-09-01 18:48:24 +0530

    reset install

--- extension/scripts/popup/app.js
+++ extension/scripts/popup/app.js
@@ -1,27 +1,19 @@
+function handleInteraction (thisbrowser) {
+	joinRoomButton.addEventListener('click', function () {
+		if(roomText.value === "")
+			return;
+		thisbrowser.tabs.query({currentWindow: true, active: true},
+			function (tabs) {
+				if(roomText.value === ""){
+					thisbrowser.tabs.sendMessage(tabs[0].id, {error: "Room name empty"});
+					return;	
+				}else{ thisbrowser.tabs.sendMessage(tabs[0].id, {roomName: roomText.value});	}
+			}
+		);
+	}, false);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-	if(chrome){
-		joinRoomButton.addEventListener('click', function () {
-			chrome.tabs.query({currentWindow: true, active: true},
-				function (tabs) {
-					if(roomText.value === ""){
-						chrome.tabs.sendMessage(tabs[0].id, {error: "Room name empty"});
-						return;	
-					}else{ chrome.tabs.sendMessage(tabs[0].id, {roomName: roomText.value});	}
-				}
-			);
-		}, false);
-	}else{
-		joinRoomButton.addEventListener('click', function () {
-			if(roomText.value === "")
-				return;
-			browser.tabs.query({currentWindow: true, active: true},
-				function (tabs) {
-					if(roomText.value === ""){
-						browser.tabs.sendMessage(tabs[0].id, {error: "Room name empty"});
-						return;	
-					}else{ browser.tabs.sendMessage(tabs[0].id, {roomName: roomText.value});	}
-				}
-			);
-		}, false);
-	}
+	if(chrome){ handleInteraction(chrome);}
+	else{ handleInteraction(browser); }	
 }, false);
