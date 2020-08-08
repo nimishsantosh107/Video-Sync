8a9113b1ac47e74a7e01d0067ab796cde4566069
nimish <nimishsantosh107@icloud.com>
2020-08-08 23:42:58 +0530

    updated xPATHS

--- extension/scripts/content/xpaths.js
+++ extension/scripts/content/xpaths.js
@@ -2,5 +2,5 @@
 function getElementByXpath(path) {return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;}
 
 //XPATHS
-const youtubeVideoFirefox = `/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[1]/video`;
+const youtubeVideoFirefox = `/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[3]/div/ytd-player/div/div/div[1]/video`;
 const youtubeVideoChrome = `//*[@id="movie_player"]/div[1]/video`;
